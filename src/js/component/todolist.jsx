9a7f2894014cb56ea95a4e75c71b0df156dbde62
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-03-04 05:08:03 +0000

    90% done

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 
 //create your first component
 export function TodoList() {
-    const [newTask, setNewTask] = useState(['Print Bills', "ROnald is better than Messi"]);
+    const [newTask, setNewTask] = useState(['Print Bills', "Ronald is better than Messi"]);
     const [tempTask, setTempTask] = useState('');
     const [undoTask, setUndoTask] = useState(0);
     const [failTask, setFailTask] = useState('');
@@ -18,24 +18,24 @@ let interval;
     useEffect(() => {
 
    
-interval= setInterval(()=>{
-    if(undoTask)
-{
-    if(counter!=0){
-        setCounter(counter-1);
-        }
-        else{
-        setUndoTask(false);
-        setFailTask('');
-        }
-    }
-    else{
+// interval= setInterval(()=>{
+//     if(undoTask)
+// {
+//     if(counter!=0){
+//         setCounter(counter-1);
+//         }
+//         else{
+//         setUndoTask(false);
+//         setFailTask('');
+//         }
+//     }
+//     else{
 
-    }
-},1000);
 
-}
-    }, [undoTask]);
+//     }
+// },1000);
+
+}, [undoTask]);
 
 
     function saveTask(val) {
@@ -44,28 +44,29 @@ interval= setInterval(()=>{
     }
 
     function add_list() {
-       // let testArray = newTask.map((element,index)=> element);
         let testArray = [... newTask];
         testArray.push(tempTask);
       
         setNewTask(testArray);
+        setTempTask('');
 
 
 
     };
 
-    function undo_item() {
+    function undo_item () {
         if(undoTask)
         setFailTask();
 
     }
 
-    function deleteTask(id) {
-        setFailTask(testArray [id]);
- let testArray = [... newTask];
+    function deleteTask (id) {
+ 
+ let testArray = [...newTask];
+ setFailTask(testArray[id]);
  delete testArray [id];
  setNewTask(testArray);
- setUndoTask(true);
+   setUndoTask(true);
  
 
     }
@@ -89,7 +90,8 @@ interval= setInterval(()=>{
                                     <h4 className="mb-3">Awesome Todo List</h4>
 
                                
-                                    <div class="add-items d-flex"> <input type="text" class="form-control todo-list-input" placeholder="What do you need to do today?" onChange={(e)=> saveTask(e)}/> <button class="add btn btn-primary font-weight-bold todo-list-add-btn"  onClick={() => add_list()}>Add</button> </div>
+                                    <div class="add-items d-flex"> <input type="text" class="form-control todo-list-input" placeholder="What do you need to do today?" onChange={(e)=> saveTask(e)}/> 
+                                    <button class="add btn btn-primary font-weight-bold todo-list-add-btn" onClick={() => add_list()}>Add</button> </div>
                      
                                         <div className="form-outline flex-fill">
                                                  <label class="form-label" htmlFor="form3">What do you need to do today?</label>
@@ -103,13 +105,13 @@ interval= setInterval(()=>{
 
                                             <li key={index}
                                                 class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                                                <div class="d-flex align-items-center">
-                                               
-                                                    {element}
-                                                </div>
-                                               <span onClick={()=>deleteTask(index)} >
+                                                <div class="d-flex align-items-center">  {element}  </div>
+
+                                                <span onClick={()=> deleteTask(index)} >
                                                     <i class="fas fa-times text-primary"></i>
-                                                </span>
+                                                </span>    
+                                               
+                                              
                                             </li>
                                         )}
 
